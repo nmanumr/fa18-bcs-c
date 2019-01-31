353e4e9d501b9579126e77bbae3a0e02f2db1542
nmanumr <nmanumr@gmail.com>
2019-01-31 12:54:02 +0500

    added firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId"
+  }
 };
 
 /*
